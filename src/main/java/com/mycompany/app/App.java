4f526124452eace2457e92a36ab400457e9599e1
Maulana <maulana@localhost.localdomain>
2025-01-23 17:59:08 +0700

    Changes Hello World

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,11 +5,12 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello World! ### Changes";
+    private static final String MESSAGE = "Hello World!";
 
     public App() {}
 
     public static void main(String[] args) {
+        System.out.println(MESSAGE);
         System.out.println(MESSAGE);
     }
 
