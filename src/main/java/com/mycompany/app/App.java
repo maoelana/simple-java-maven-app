680573ce89f7cecdcdb5aa3683fc3d90561c4fc7
Maulana <maulana@localhost.localdomain>
2025-01-23 20:56:06 +0700

    Changes Hello World

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -12,6 +12,9 @@ public class App {
     public static void main(String[] args) {
         System.out.println(MESSAGE);
         System.out.println(MESSAGE);
+        System.out.println(MESSAGE);
+        System.out.println(MESSAGE);
+        System.out.println(MESSAGE);
     }
 
     public String getMessage() {
